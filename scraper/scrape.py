2e130a9450af738769045c287510277a2e67d649
Resocram <marcoser2000@gmail.com>
2023-04-05 12:41:13 -0700

    Fixed env path

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -3,12 +3,13 @@ from syncKendra import syncKendra
 import os
 from dotenv import load_dotenv
 from uploadS3 import uploadS3
+from pathlib import Path
 
-load_dotenv()
+load_dotenv(dotenv_path=Path("../src/.env"))
 bucket_name = os.getenv("S3_BUCKET_NAME")
 kendra_data_source_id = os.getenv("KENDRA_DATA_SOURCE_ID")
 kendra_index_id = os.getenv("KENDRA_INDEX_ID")
 profile_name = os.getenv("PROFILE_NAME")
 
 beginScraper(bucket_name,profile_name)
-syncKendra(kendra_data_source_id,kendra_index_id)
+syncKendra(kendra_data_source_id,kendra_index_id,profile_name)
